9494ac79387952c3214914bff54f8b58ccbc2c53
Andrei Dragomir <dragomirandy900@yahoo.com>
2019-04-06 22:02:33 +0300

    ceva chestii

--- javascript/menu.js
+++ javascript/menu.js
@@ -11,3 +11,16 @@ function toggleMenu(){
     console.log("merge");
     document.querySelector('ul').classList.toggle('active');
 }
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("avatar");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
