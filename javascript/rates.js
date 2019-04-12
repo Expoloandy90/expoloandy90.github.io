74be0b9bdf0c72bcfef59cfb6802d9a367efa705
Andrei Dragomir <dragomirandy900@yahoo.com>
2019-04-12 23:57:25 +0300

    color text

--- javascript/rates.js
+++ javascript/rates.js
@@ -2,25 +2,15 @@
 var table = document.getElementById("myTable");
 var currentcy = ["RON", "EUR", "USD", "BGN", "GBP", "RUB", "CAD", "JPY", "CHF", "TRY", "HRK", "NOK", "DKK", "CZK", "HUF", "ISK", "PLN", "SEK"];
 var base = "EUR";
-var data;
-var url ="https://api.exchangeratesapi.io/";
+var url = "https://api.exchangeratesapi.io/";
 
 var xmlhttp = new XMLHttpRequest();
 
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         // Begin accessing JSON data here
-        data = JSON.parse(this.responseText);
-        var table = document.getElementById("myTable");
-        for (var i = 0; i < currentcy.length; i++) {
-            if (currentcy[i] != "EUR") {
-                var row = table.insertRow(table.rows.length);
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                cell1.innerHTML = currentcy[i];
-                cell2.innerHTML = data.rates[currentcy[i]];
-            }
-        }
+        var data = JSON.parse(this.responseText);
+        compareValues(data);
         //Urmeaza sa fac o functie care arata cum a evoluat pretul de ieri pana azi
         //O functie care returneaza data de ieri in format 2019-04-11
     }
@@ -28,7 +18,39 @@ xmlhttp.onreadystatechange = function () {
 xmlhttp.open("GET", url + 'latest?base=' + base, true);
 xmlhttp.send();
 
-var yesterday = function (date1) {
+var compareValues = function (today) {
+    console.log(getYesterday(today.date));
+    var yesterdayDate = getYesterday(today.date).getFullYear() + '-' + getYesterday(today.date).getMonth() + '-' + getYesterday(today.date).getDate();
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // Begin accessing JSON data here
+            var yesterdayData = JSON.parse(this.responseText);
+            var table = document.getElementById("myTable");
+            for (var i = 0; i < currentcy.length; i++) {
+                if (currentcy[i] != "EUR") {
+                    var row = table.insertRow(table.rows.length);
+                    var cell1 = row.insertCell(0);
+                    var cell2 = row.insertCell(1);
+                    var cell3 = row.insertCell(2);
+                    cell1.innerHTML = currentcy[i];
+                    cell2.innerHTML = yesterdayData.rates[currentcy[i]];
+                    if(today.rates[currentcy[i]] > yesterdayData.rates[currentcy[i]])
+                        cell2.style.color = "green";
+                    else if(today.rates[currentcy[i]] < yesterdayData.rates[currentcy[i]])
+                            cell2.style.color = "red";
+                }
+            }
+            //Urmeaza sa fac o functie care arata cum a evoluat pretul de ieri pana azi
+            //O functie care returneaza data de ieri in format 2019-04-11
+        }
+    };
+    xmlhttp.open("GET", url + yesterdayDate + '?base=' + base, true);
+    xmlhttp.send();
+}
+
+var getYesterday = function (date1) {
     var dt = new Date(date1);
-    return new Date((dt.setDate(dt.getDate() - 1))).toString();
+    return new Date((dt.setDate(dt.getDate() - 1)));
 }
